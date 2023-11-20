028e6020f316951b9b8b81538863a8bf269f15b0
priscilla-02 <136003704+priscilla-02@users.noreply.github.com>
2023-11-20 15:47:30 +0000

    amend task 2

--- app.js
+++ app.js
@@ -2,14 +2,10 @@ const express = require("express");
 const app = express();
 const { getTopics } = require("./servers.controllers");
 
-app.use(express.json());
-
 app.get("/api/topics", getTopics);
 
 app.use((err, req, res, next) => {
-  if (err.code === "22P02") {
-    res.status(400).send({ msg: "bad request" });
-  } else if (err.status) {
+  if (err.status) {
     res.status(err.status).send({ msg: err.msg });
   } else {
     res.status(500).send({ msg: "internal server error" });
--- servers.models.js
+++ servers.models.js
@@ -1,7 +1,7 @@
 const db = require("./db/connection");
 
 exports.getAllTopics = () => {
-  let queryString = `SELECT topics.* FROM topics`;
+  let queryString = `SELECT * FROM topics`;
   return db.query(queryString).then(({ rows }) => {
     return rows;
   });
