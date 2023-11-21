c7b3deee419666d2c2df96b2a6f00c86672e0add
priscilla-02 <136003704+priscilla-02@users.noreply.github.com>
2023-11-21 13:50:05 +0000

    revise task 4 error handling test

--- servers.models.js
+++ servers.models.js
@@ -13,3 +13,16 @@ exports.getObjByEndpoints = () => {
     return objEndpoints;
   });
 };
+
+exports.getAllArticles = (article_id) => {
+  return db
+    .query(`SELECT * FROM articles WHERE article_id = $1`, [article_id])
+    .then((result) => {
+      const article = result.rows[0];
+      if (!article) {
+        return Promise.reject({ status: 404, msg: "Not Found" });
+      }
+
+      return article;
+    });
+};
