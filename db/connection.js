934ff5cf6ebc0a0693bfbf6b90519a65be1bb410
priscilla-02 <136003704+priscilla-02@users.noreply.github.com>
2023-11-23 14:34:06 +0000

    edit process.env.DATABASE_URL, add config variable

--- db/connection.js
+++ db/connection.js
@@ -5,8 +5,17 @@ require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 // console.log(process.env.PGDATABASE);
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE not set");
 }
 
 module.exports = new Pool();
+
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
+}
+
+module.exports = new Pool(config);
